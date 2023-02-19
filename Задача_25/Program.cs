95bcbfe5d4cfa7f3fae001be4c800bf29a814dba
Artur <ninicu.artur@mail.ru>
2023-02-19 20:30:46 +0200

    25

---
+++ Задача_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+double MathPow(int num, int st)
+{
+    double n = Math.Pow(num, st);
+    return n;
+}
+
+double math = MathPow(A, B);
+Console.WriteLine($"{A}, {B} -> {math}");
---
+++ Задача_48/Program.cs
@@ -0,0 +1,36 @@
+﻿// 
+
+Console.Clear();
+
+int[,] GetArray(int row, int col)
+{
+    int[,] arroy = new int[row, col];
+    for (int i = 0; i < row; i++);
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите размер строк: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите размер столбцов: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, col);
+PrintArray(array);
